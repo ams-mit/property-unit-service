2c68de8c947a9c5f7c4756a2b126a95338eedea2
ThathsaraDasun <dasunh2004@gmail.com>
2026-09-25 12:00:04 +0530

    Fix syntax error in PropertyUnitServiceApplication

--- src/main/java/lk/ac/kln/property_unit_service/PropertyUnitServiceApplication.java
+++ src/main/java/lk/ac/kln/property_unit_service/PropertyUnitServiceApplication.java
@@ -2,18 +2,20 @@ package lk.ac.kln.property_unit_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Put the new imports up here with the others
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-@Bean
-public RestTemplate restTemplate() {
-    return new RestTemplate();
-}
 
 @SpringBootApplication
 public class PropertyUnitServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PropertyUnitServiceApplication.class, args);
+	}
+
+	// The Bean method MUST go inside the class's curly braces
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
